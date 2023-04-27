512d9bf36cefdfcabdd3d20de3b578c81072d570
J <skywlstn777@gmail.com>
2023-04-27 10:38:27 +0900

    #1 feat: 스프링 프로젝트 기본값 설정

---
+++ src/main/java/com/gk_board/GkBoardApplication.java
@@ -0,0 +1,13 @@
+package com.gk_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GkBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GkBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gk_board/GkBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gk_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GkBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
