4c02c2d53dc0bf6e720eec87fa58ef991615f48d
frontLine-kim <57103133+frontLine-kim@users.noreply.github.com>
2023-04-28 01:04:23 +0900

    #2 로우 레벨로 구현한 연관 게시글
    현재 굉장히 로우레벨의 로직으로 연관 게시글 구현 고쳐야 할 부분이 많이 있으나 처음부터 다 계산하여 로직을 짜기에는 너무 버거워서 한 스텝씩 발전시키기로 함

--- src/main/java/com/gk_board/article/service/ArticleService.java
+++ src/main/java/com/gk_board/article/service/ArticleService.java
@@ -1,12 +1,20 @@
 package com.gk_board.article.service;
 
 import com.gk_board.article.dto.ArticleDto;
+import com.gk_board.article.dto.response.ArticleResponse;
+import com.gk_board.article.entity.Article;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public interface ArticleService {
     //게시물 작성
     ArticleDto saveArticle(ArticleDto dto);
 
+    ArticleDto saveRefArticle(Long articleId, List<String> keywords);
+
     Page<ArticleDto> getArticlesList(Pageable pageable);
+
+    ArticleResponse getArticle(Long articleId);
 }
--- src/main/java/com/gk_board/article/service/impl/ArticleServiceImpl.java
+++ src/main/java/com/gk_board/article/service/impl/ArticleServiceImpl.java
@@ -1,33 +1,83 @@
 package com.gk_board.article.service.impl;
 
 import com.gk_board.article.dto.ArticleDto;
+import com.gk_board.article.dto.response.ArticleResponse;
+import com.gk_board.article.entity.Article;
+import com.gk_board.article.entity.Keyword;
 import com.gk_board.article.repository.ArticleRepository;
+import com.gk_board.article.repository.KeywordRepository;
 import com.gk_board.article.service.ArticleService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Slf4j
 @RequiredArgsConstructor
 @Transactional
 @Service
 public class ArticleServiceImpl implements ArticleService {
 
     private final ArticleRepository articleRepository;
-
+    private final KeywordRepository keywordRepository;
     //게시물 작성
     @Override
     public ArticleDto saveArticle(ArticleDto dto){
 
         ArticleDto articleDto = ArticleDto.from(articleRepository.save(dto.toEntity()));
+
         return articleDto;
     }
 
     @Override
+    public ArticleDto saveRefArticle(Long articleId, List<String> keywords){
+        Article article = articleRepository.getReferenceById(articleId);
+        List<Keyword> articleList = keywordRepository.findByContentIn(keywords);
+
+        //키워드에서 중복을 제거하고 검색
+        Set<Long> refArticleId = new HashSet<>();
+        for(int i = 0; i<articleList.size(); i++) {
+            if(!articleId.equals(articleList.get(i).getArticleId()))
+            refArticleId.add(articleList.get(i).getArticleId());
+        }
+
+        for(int i = 0; i<articleList.size(); i++){
+            if(!articleId.equals(articleList.get(i).getArticleId()))
+                article = articleRepository.getReferenceById(articleList.get(i).getArticleId());
+            article.setMainArticle(articleRepository.getReferenceById(articleId));
+        }
+
+
+
+//        List<Article> articleDtoList = new ArrayList<>();
+//        for (Long aLong : refArticleId) {
+//            articleDtoList.add(articleRepository.getReferenceById(aLong));
+//        }
+//
+//        article.setRefArticle(articleDtoList);
+        List<ArticleDto> checkArticle = articleRepository.findById(articleId).map(ArticleDto::from).stream().toList();
+
+
+        log.info("아티클 확인: {} , 아티클 리스트 확인 : {}, 연관 아티클 Id: {} , 연관 아티클 확인 : {}", article.getContent(),articleList,refArticleId, checkArticle );
+
+        return null;
+    }
+
+
+    @Override
     public Page<ArticleDto> getArticlesList(Pageable pageable){
-        Page<ArticleDto> articleList = articleRepository.findAll(pageable).map(ArticleDto::from);
-        return articleList;
+        return articleRepository.findAll(pageable).map(ArticleDto::from);
+    }
+
+    @Override
+    public ArticleResponse getArticle(Long articleId){
+        Article article = articleRepository.getReferenceById(articleId);
+        return ArticleResponse.from(article);
     }
 
 
